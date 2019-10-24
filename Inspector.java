e4ad301b45715514e596270069e95f37ae6afc7a
nathanouu <37126596+nathanouu@users.noreply.github.com>
2019-10-24 11:44:09 -0600

    Code to obtain the declaring class as well as the superclass

--- Inspector.java
+++ Inspector.java
@@ -13,7 +13,13 @@ public class Inspector
 {
 	public void inspect(Object obj, boolean recursive)
 	{
-		
+		Class objClass = obj.getClass();
+        String declaringClass = objClass.getName();
+        System.out.println("Declaring Class: " + declaringClass);
+
+
+        String superClass = objClass.getSuperclass().getName();
+        System.out.println("Super Class: " + superClass);
 	}
 }
 
