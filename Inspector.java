6187f03bcdf0d06bf8f07b328fa7430ce68ef33e
nathanouu <37126596+nathanouu@users.noreply.github.com>
2019-10-24 00:38:05 -0600

    Empty Inspector class and method

---
+++ Inspector.java
@@ -0,0 +1,20 @@
+/** 
+CPSC 501 Assignment 2
+Nathan Ou 10079578
+**/
+
+
+
+import java.lang.reflect.*;
+import java.util.*;
+
+
+public class Inspector
+{
+	public void inspect(Object obj, boolean recursive)
+	{
+		
+	}
+}
+
+
